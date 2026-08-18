e4467d8f40d9653f2eddc34c595d3ad1c3c44b3c
Sahil Verma <domainsahil007@gmail.com>
2026-08-19 00:02:07 +0530

    refactor: migrate Pinecone learning examples from embedding-pipeline.ts to learn-pinecone.ts

--- src/learn-pinecone.ts
+++ src/learn-pinecone.ts
@@ -1,22 +1,4 @@
-import path from "path";
-import { PDFLoader } from "@langchain/community/document_loaders/fs/pdf";
 import { pc } from "./db/pinecone";
-
-async function loadLocalPDF() {
-  // 1. Supply the path to your local file
-  const pdfPath = path.join(import.meta.dir, "data/BNS.pdf");
-  const loader = new PDFLoader(pdfPath, {
-    splitPages: true, // true (default) splits pages into separate documents
-  });
-
-  // 2. Parse the PDF into LangChain Documents
-  const docs = await loader.load();
-
-  // 3. View the results
-  console.log(`Loaded ${docs.length} pages.`);
-  console.log("First page content snippet:", docs[0]!.pageContent);
-  //   console.log("Metadata of first page:", docs[0]!.metadata);
-}
 
 // loadLocalPDF();
 
